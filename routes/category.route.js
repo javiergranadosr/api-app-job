c41d0fd263642ebf6f03c7f359912dc59385c8d6
javiergranadosr <javiergranadosrdev@gmail.com>
2022-08-06 20:30:38 -0500

    Se modifica ruta categorias

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,14 +1,10 @@
 const { Router } = require("express");
-const { validateJwt } = require("../middlewares/validate.jwt");
-const { hasRole } = require("../middlewares/validate.role");
-const validate = require("../middlewares/validate");
 const { getCategories } = require("../controllers/index.controller");
 
 const router = Router();
 
 router.get(
   "/",
-  [validateJwt, hasRole("RECRUITER"), validate],
   getCategories
 );
 
